45d8ba03427691c7c7e81fea759050d699ec50a5
Steven Maillet <smaillet@users.noreply.github.com>
2026-01-21 13:40:02 -0800

    Doc updates and refactoring (#30)
    * Updated markdown based on linter
        - Linter ultimately abandoned as it is too inflexible and not consistent
    * Refactored getting the named type and context validation to `CommandGenerator.TryGetNamedTypeSymbol()`
    * Added use of `Microsoft.CodeAnalysis.CSharp` for literal conversions
    * Added direct testing of the language specific generator support

---
+++ src/Ubiquity.NET.SrcGeneration.UT/CSharp/LanguageTests.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Ubiquity.NET Contributors. All rights reserved.
+// Licensed under the Apache-2.0 WITH LLVM-exception license. See the LICENSE.md file in the project root for full license information.
+
+namespace Ubiquity.NET.SrcGeneration.UT.CSharp
+{
+    [TestClass]
+    public class LanguageTests
+    {
+        [TestMethod]
+        public void Literal_values_are_correct( )
+        {
+            Assert.AreEqual( "{", CSharpLanguage.ScopeOpen );
+            Assert.AreEqual( "}", CSharpLanguage.ScopeClose );
+
+            Assert.HasCount( ExpectedKeywords.Length, CSharpLanguage.KeyWords );
+            Assert.IsTrue( ExpectedKeywords.SequenceEqual( CSharpLanguage.KeyWords ) );
+
+            Assert.AreEqual( "true", CSharpLanguage.AsLiteral( true ) );
+            Assert.AreEqual( "false", CSharpLanguage.AsLiteral( false ) );
+
+            // Escaping of strings is... challenging. The escaped literal form
+            // includes the escaping characters so testing for that requires escaping the escape chars...
+            Assert.AreEqual( "\"this is a \\\"test\\\"\"", CSharpLanguage.AsLiteral( "this is a \"test\"" ) );
+
+            Assert.AreEqual( "'\u0124'", CSharpLanguage.AsLiteral( '\u0124' ) );
+            Assert.AreEqual( "'\\u2028'", CSharpLanguage.AsLiteral( '\u2028' ) );
+
+            Assert.AreEqual( $"@{ExpectedKeywords[ 0 ]}", CSharpLanguage.MakeIdentifier( ExpectedKeywords[ 0 ] ) );
+            Assert.AreEqual( "foo_bar", CSharpLanguage.MakeIdentifier( "foo bar" ) );
+        }
+
+        private readonly ImmutableArray<string> ExpectedKeywords
+            = [ // Source: Language spec. §6.4.4 Keywords
+                "abstract",
+                "as",
+                "base",
+                "bool",
+                "break",
+                "byte",
+                "case",
+                "catch",
+                "char",
+                "checked",
+                "class",
+                "const",
+                "continue",
+                "decimal",
+                "default",
+                "delegate",
+                "do",
+                "double",
+                "else",
+                "enum",
+                "event",
+                "explicit",
+                "extern",
+                "false",
+                "finally",
+                "fixed",
+                "float",
+                "for",
+                "foreach",
+                "goto",
+                "if",
+                "implicit",
+                "in",
+                "int",
+                "interface",
+                "internal",
+                "is",
+                "lock",
+                "long",
+                "namespace",
+                "new",
+                "null",
+                "object",
+                "operator",
+                "out",
+                "override",
+                "params",
+                "private",
+                "protected",
+                "public",
+                "readonly",
+                "ref",
+                "return",
+                "sbyte",
+                "sealed",
+                "short",
+                "sizeof",
+                "stackalloc",
+                "static",
+                "string",
+                "struct",
+                "switch",
+                "this",
+                "throw",
+                "true",
+                "try",
+                "typeof",
+                "uint",
+                "ulong",
+                "unchecked",
+                "unsafe",
+                "ushort",
+                "using",
+                "virtual",
+                "void",
+                "volatile",
+                "while"
+              ];
+    }
+}
--- src/Ubiquity.NET.SrcGeneration.UT/GlobalNamespaceImports.cs
+++ src/Ubiquity.NET.SrcGeneration.UT/GlobalNamespaceImports.cs
@@ -15,9 +15,11 @@ For an explanation of the benefits of the language feature see: https://www.hans
 global using System;
 global using System.CodeDom.Compiler;
 global using System.Collections.Generic;
+global using System.Collections.Immutable;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
 global using System.IO;
+global using System.Linq;
 
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 
